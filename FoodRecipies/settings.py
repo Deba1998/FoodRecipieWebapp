ba8e32643ede92c59be2a5e25ed2fbc95c1cd737
Debajyoti Saha <debajyotisaha1998@gmail.com>
2022-07-12 20:05:26 +0530

    modified setting file for statistics

--- FoodRecipies/settings.py
+++ FoodRecipies/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = 'static/'
 # Added manually
